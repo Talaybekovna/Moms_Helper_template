e1d723695da04fde947c564e13041109875aa0f0
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-18 22:12:02 +0600

    set DatePickerDialog

--- app/src/main/java/kg/tutorialapp/momshelper_template/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/momshelper_template/MainActivity.kt
@@ -1,11 +1,53 @@
 package kg.tutorialapp.momshelper_template
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn_date: Button
+    val cal = Calendar.getInstance()
+    lateinit var dateOfBirth: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_date = findViewById(R.id.btn_dateOfBirth)
+        setup()
+    }
+
+    private fun setup() {
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, month)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        btn_date.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                DatePickerDialog(this@MainActivity,
+                        dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        })
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        dateOfBirth = sdf.format(cal.time)
     }
 }
